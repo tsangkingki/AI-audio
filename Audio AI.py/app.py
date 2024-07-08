16f5ea0ec19cd11461fd645dbe662d25504abecf
NoahTsang <tsangkingki@DESKTOP-LQM0GA8>
2024-07-08 13:14:30 +0800

    no response Llama3

--- Audio AI.py/app.py
+++ Audio AI.py/app.py
@@ -1,41 +1,65 @@
 # audio to text
 # from dotenv import find_dotenv, load_dot_env
 from transformers import pipeline
-from huggingface_hub import InferenceClient
+from transformers import AutoTokenizer, AutoModelForCausalLM,AutoModel
 import torch
-import sysconfig; print(sysconfig.get_paths()["purelib"])
-import _socket
+from huggingface_hub import InferenceClient
+from huggingface_hub import login
+import torch
+
+api_token ="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"
+
+# Log in to the Hugging Face model hub
+login(api_token)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+print(torch.cuda.get_device_name(device))
+print("Default device:", device)
+
 def audioToText(url):
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
-    print(torch.cuda.get_device_name(device))
-    data, samplerate = sf.read(url) 
-    print("samplerate: ",samplerate);
-    print("Default device:", device)
     pipe = pipeline("automatic-speech-recognition", model="alvanlii/distil-whisper-small-cantonese",device=device)
-    text = pipe(data)
+    text = pipe(url)
     return text
 
-result =audioToText("test.mp3");
-print(result)
-# load_dotenv(find_dotenv())
+# ASRoutput =audioToText("test.mp3");
+# print(ASRoutput)
 
-# def llm():
-#     if torch.cuda.is_available():
-#         device = torch.device("cuda")
-#     else:
-#         device = torch.device("cpu")
-#     print(torch.cuda.get_device_name(device))
-#     print("Default device:", device)
-#     messages = [
-#     {"role": "user", "content": "Who are you?"},]
-#     pipe = pipeline("text-generation", model="microsoft/Phi-3-mini-4k-instruct", trust_remote_code=True,device=device)
-#     pipe(messages)
+def llm(messages):
+    #load the model
+    model = AutoModel.from_pretrained("backyardai/llama-3-8b-Instruct-GGUF")
+    pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3-8B-Instruct",device_map="auto",model_kwargs={"torch_dtype": torch.bfloat16})
+    #prompt template
+    prompt = pipe.tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt = True,
+    )
+    print("prompt",prompt)
+    #identify the end of text
+    terminators = [
+        pipe.tokenizer.eos_token_id,
+        pipe.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+    ]
+    torch.cuda.empty_cache()
+    response = pipe(
+        prompt,
+        max_new_tokens= 256,
+        eos_token_id = terminators,
+        do_sample = True, # text generation stragaries
+        temperature = 0.6,
+        top_p = 0,
+    )
+    print(response)
+    print("dddddddddd")
+    print(response[0]["generated_text"][len(prompt):])
+    return response
 
-# llm();
-
+messages = [{"role": "user", "content": "Who are you?"}]
+llm_output = llm(messages)
+print(llm_output)
 # llm
 
 #text to audio
